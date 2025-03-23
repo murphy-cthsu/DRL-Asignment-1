dbf2a22615dd8495de7ce7e9d8c1bc60af8a9527
murphy-cthsu <murphy.cthsu@gmail.com>
2025-03-23 19:30:05 +0800

    hope so

--- student_agent.py
+++ student_agent.py
@@ -4,9 +4,12 @@ import torch.nn.functional as F
 import numpy as np
 import os
 import random
+
+
 from policy_agent import PolicyAgent
 
-agent=PolicyAgent('trained_models/policy_model_03.pth')
+agent=PolicyAgent('trained_models/policy_model.pth')
 
 def get_action(obs):
+
     return agent.get_action(obs)
